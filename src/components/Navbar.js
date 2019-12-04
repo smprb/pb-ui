19f859eeb8b35e92abbda2c18d7bce41ded6a98b
Murat Slavich <Murat.Slavich@sidenis.com>
2019-12-05 02:02:34 +0700

    add css modules

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,21 +1,22 @@
 import React from 'react';
+import s from './Navbar.module.css';
 
 const Navbar = () => {
     return (
-        <nav className='nav'>
-            <div>
+        <nav className={s.nav}>
+            <div className='item'>
                 <a>Profile</a>
             </div>
-            <div>
+            <div className='item'>
                 <a>Messages</a>
             </div>
-            <div>
+            <div className='item'>
                 <a>News</a>
             </div>
-            <div>
+            <div className='item'>
                 <a>Music</a>
             </div>
-            <div>
+            <div className='item'>
                 <a>Settings</a>
             </div>
         </nav>
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import s from './Profile.module.css'
 
 const Profile = () => {
     return (
