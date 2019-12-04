7f59c3db9a49ee71c5fd6fc062075e7448caf40a
Murat Slavich <Murat.Slavich@sidenis.com>
2019-12-04 22:14:23 +0700

    define main structure

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className='header'>
+            <img src='public\square.png' />
+        </header>
+    )
+}
+
+export default Header;
