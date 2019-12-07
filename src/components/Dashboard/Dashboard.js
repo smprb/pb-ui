69609c9a0bc25b01fbd15b641fa616e3db7b1267
Murat Slavich <Murat.Slavich@sidenis.com>
2019-12-08 03:28:52 +0700

    change display to flex

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,14 +1,27 @@
 import React from "react";
 
 const dashboardStyle = {
-    'grid-area': 'dashboard',
+    'flex': '3 0px',
+    'order': '2',
     'background-color': 'white',
     'border-bottom': '2px solid rgba(0,0,0,0.15)'
 
 };
 
+const ToDoList = () => {
+    return (
+        <div>
+            ToDoList
+        </div>
+    )
+}
+
 const Dashboard = () => {
-    return <div style={dashboardStyle}>Dashboard</div>;
+    return (
+        <div style={dashboardStyle}>
+            <ToDoList />
+        </div>
+    )
 }
 
 export default Dashboard;
