e79abdfcecefa390be188ea043aaa72660e42ace
Murat Slavich <Murat.Slavich@sidenis.com>
2019-12-08 21:14:11 +0700

    fix main background color

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -2,16 +2,43 @@ import React from "react";
 
 const dashboardStyle = {
     'flex': '3 0px',
-    'order': '2',
-    'background-color': 'white',
-    'border-bottom': '2px solid rgba(0,0,0,0.15)'
-
+    'order': '2'
 };
+
+const item = {
+    'background-color': 'white',
+    'margin-bottom': '10px',
+    'border-bottom': '2px solid rgba(0,0,0,0.15)'
+}
 
 const ToDoList = () => {
     return (
-        <div>
+        <div style={item}>
             ToDoList
+        </div>
+    )
+}
+
+const Suggestion = () => {
+    return(
+        <div style={item}>
+            Suggestion
+        </div>
+    )
+}
+
+const Plans = () => {
+    return(
+        <div style={item}>
+            Plans
+        </div>
+    )
+}
+
+const Posts = () => {
+    return(
+        <div style={item}>
+            Posts
         </div>
     )
 }
@@ -20,6 +47,9 @@ const Dashboard = () => {
     return (
         <div style={dashboardStyle}>
             <ToDoList />
+            <Suggestion />
+            <Plans />
+            <Posts />
         </div>
     )
 }
