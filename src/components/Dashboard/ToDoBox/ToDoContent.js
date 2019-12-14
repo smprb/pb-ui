b4a585b697ece24066b6fb54071477125a15ab59
Murat Slavich <Murat.Slavich@sidenis.com>
2019-12-14 15:37:23 +0700

    add docker

--- src/components/Dashboard/ToDoBox/ToDoContent.js
+++ src/components/Dashboard/ToDoBox/ToDoContent.js
@@ -1,26 +1,37 @@
 import store from "../../../store";
 import render from "../../../index";
 import styles from "../Dashboard.module.css";
-import React from "react";
+import React, { useState } from "react";
 import TodoItem from "./ToDoItem";
+import api from "./../../../utils/api"
 
 export const TodoContent = () => {
 
-    let state = store.getState();
-    let items = state.todolist.map((it) => <TodoItem text={it.text}/>);
-    const dispatch = store.dispatch;
-    store.subscribe(() => {
-        render.apply()
-    });
+    //let state = store.getState();
+    // let items = state.todolist.map((it) => <TodoItem text={it.text}/>);
+    // const dispatch = store.dispatch;
+    // store.subscribe(() => {
+    //     render.apply()
+    // });
 
-    const check = () => {
-        dispatch({type: 'ADD'})
-    }
+    // const check = () => {
+    //     dispatch({type: 'ADD'})
+    // }
+
+    let useData;
+    api.get("/foo")
+        .then(response => useData = response.data);
+
+    const [items, addItem] = useState([]);
+
+    const addEntry = () => {
+        addItem(oldArray => [...oldArray, `${useData.field} ${oldArray.length}`])
+    };
 
     return (
         <div className={styles.content}>
-            {items}
-            <div onClick={check} className={styles.button}>
+            {items.map(entry => <TodoItem text={entry}/>)}
+            <div onClick={addEntry} className={styles.button}>
                 Add
             </div>
         </div>
